2465ba06b4b782d43f06eb114597b30b5e63478b
Nick Brown <n.brown.professional@gmail.com>
2018-03-05 21:14:26 -0700

    Adds start to AJAX/API calls

---
+++ assets/logic.js
@@ -0,0 +1,48 @@
+
+$(document).ready(function () {
+
+    // 6 character
+    // Huklk, ironman, captain america, hawkeye, thor, black widdow
+
+    // http://www.omdbapi.com/?s=thor&year=2008&apikey=f367c400
+
+    // API/AJAX Test sections  
+
+    //OMDB Key = f367c400
+    //Marvel API Key = ded1811505155fd04255b903b7f0378a
+
+    var characterName = "hulk"; // will be set during the onclick event to whatever the items data-name attr is 
+    var marvelQueryURL = "";
+    var OMDBQueryURL = "http://www.omdbapi.com/?s=" + characterName + "&year=2008&apikey=f367c400";
+
+    $(".char").on("click", "img", function () {
+
+        ß
+        $.ajax({
+            url: marvelQueryURL,
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+
+        });
+
+        $.ajax({
+            url: OMDBQueryURL,
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+        });
+
+    })
+
+
+
+
+    // For 6 main page buttons, when clicked on will input a data value with the character name to input
+
+    // For input box, will input the value into the query
+
+
+
+
+});
